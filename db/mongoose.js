993b2f289af3282dac7651dbf32e852e362f9d41
Marlene Bowles <marlenebowles@gmail.com>
2019-11-12 10:23:51 -0500

    Figured out what was causing env-cmd to crash

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const mongoDbUri = process.env.MONGODB_URI;
 const mongoDbName = process.env.MONGODB_NAME
 
-console.log(`${mongoDbUri}/${mongoDbName}`)
 mongoose.connect(`${mongoDbUri}/${mongoDbName}`, {
     useNewUrlParser:true,
     useCreateIndex: true,
