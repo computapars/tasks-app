280850255d1e804bd363c0c6831545cb3394f302
Marlene Bowles <marlenebowles@gmail.com>
2019-11-15 16:14:37 -0500

    Adds zeit package for deployment

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const mongoDbUri = process.env.MONGODB_URI;
 const mongoDbName = process.env.MONGODB_NAME
 
+
 mongoose.connect(`${mongoDbUri}/${mongoDbName}`, {
     useNewUrlParser:true,
     useCreateIndex: true,
