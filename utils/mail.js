2290db9a808941bc1bd7594f6d87870697fdabaf
Marlene Bowles <marlenebowles@gmail.com>
2019-11-15 14:50:24 -0500

    Renamed app to yourturn

--- utils/mail.js
+++ utils/mail.js
@@ -8,10 +8,10 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const welcomeEmail = (email, name) => {
     const msg = {
         to: email,
-        from: 'taskify@taskify.dev',
-        subject: `${name}, welcome to taskify.dev`,
-        text: `Welcome to the app Taskify.dev an easy place to share tasks with friends and family.`,
-        html: `<strong>Welcome</strong><br>Login to your new profile at taskify.dev</br>
+        from: 'welcome@yourturn.dev',
+        subject: `${name}, welcome to yourturn.dev`,
+        text: `Welcome to the app YourTurn.dev, an easy place to share tasks with friends and family.`,
+        html: `<strong>Welcome</strong><br>Login to your new profile at yourturn.dev</br>
         to get started adding tasks and getting stuff done.`,
     };
     sgMail.send(msg);
@@ -20,9 +20,9 @@ const welcomeEmail = (email, name) => {
 const inviteToHouseEmail = ({ referral, email, name, message, houseName, houseId}) => {
     const msg = {
         to: email,
-        from: 'taskify@taskify.dev',
-        subject: `${referral} has invited you to join ${houseName} at Taskify.dev`,
-        text: `${referral} has invited you to join ${houseName} at Taskify.dev`,
+        from: 'invite@yourturn.dev',
+        subject: `${referral} has invited you to join ${houseName} at YourTurn.dev`,
+        text: `${referral} has invited you to join ${houseName} at YourTurn.dev`,
         html: `Hello ${name}, <br>${message}</br>
         <a href="http://${url}/house/members/invite/?houseId=${houseId}&houseName${houseName}&userName=${name}">
         Create Your Profile</a> now and start getting stuff done!
