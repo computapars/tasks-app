1c51e27e7864cb1ec365d3d75114151360a605c9
Computapars <computapars@gmail.com>
2019-11-15 19:00:12 -0500

    Trying to figure out if this is a mongoose issue

--- db/mongoose.js
+++ db/mongoose.js
@@ -6,7 +6,6 @@ const mongoDbName = process.env.MONGODB_NAME
 mongoose.connect(`${mongoDbUri}/${mongoDbName}`, {
     useNewUrlParser:true,
     useCreateIndex: true,
-    useUnifiedTopology: true,
 });
 
 mongoose.set('useFindAndModify', false);
